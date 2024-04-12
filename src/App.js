32866dcb8196a348173b750371574abc1c2010ff
Ankit Kumar <ankitkumar.lnmiit@gmail.com>
2024-04-12 02:36:29 -0700

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer =  setInterval(() => {
+      setDate(new Date())
+    }, 100);
+
+    return () => {
+      window.clearInterval(timer);
+    }
+  }, [])
+
+  const hrs = date.getHours();
+  const mins = date.getMinutes();
+  const seconds = date.getSeconds();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='time-wrapper'>
+        <span>{hrs}</span>
+        :
+        <span>{mins}</span>
+        :
+        <span>{seconds}</span>
+      </p>
     </div>
   );
 }
